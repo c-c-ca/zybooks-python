90b96e2893440a4cbd90665d60066fbb610f9a17
Adam Carter <admca@DESKTOP-TP8OTIH.localdomain>
2022-09-06 18:21:14 -0400

    Change variable name in square/cube exercise

--- ch1/c01e10/script.py
+++ ch1/c01e10/script.py
@@ -7,7 +7,7 @@ print("You entered:", user_num)
 print(user_num, "squared is", user_num ** 2)
 print("And", user_num, "cubed is", user_num ** 3, "!!")
 
-another_num = int(input("Enter another integer:\n"))
+user_num2 = int(input("Enter another integer:\n"))
 
-print(user_num, "+", another_num, "is", user_num + another_num)
-print(user_num, "*", another_num, "is", user_num * another_num)
+print(user_num, "+", user_num2, "is", user_num + user_num2)
+print(user_num, "*", user_num2, "is", user_num * user_num2)
